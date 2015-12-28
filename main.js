a2fc656062549c36813d054ef94d53ceeccd3a13
ken-okada <ken.okada@playnext.com>
2015-12-28 11:59:27 -0800

    change extension of views from .ejs to .html

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.engine('.html', require('ejs').__express);
 app.set('port', 3000);
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
